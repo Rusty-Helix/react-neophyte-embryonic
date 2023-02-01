76a932e91c130d6b378269bb1dc3fc91b716dab9
Rusty-Helix <a0900492616@gmail.com>
2023-02-01 19:24:46 +0800

    2nd 0201

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// all component files start with a capitalized letter
+
+// import React from 'react' // used to be required
+
+const Header = () => {
+    return (
+            <header>
+                <h1>Task Tracker</h1>
+            </header>
+        )
+}
+
+export default Header
