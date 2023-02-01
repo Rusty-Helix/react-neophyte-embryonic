fe5e5f205317426f97c493ff7bc24c0316c18615
Rusty-Helix <a0900492616@gmail.com>
2023-02-01 21:42:45 +0800

    0201 0942

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/Header'
+import './index.css'
 
 
 // App is also a component
-function App() { // same as the file name
+const App = () => { // same as the file name
 
 
   return (
     // this can only return 1 element
     <div className="Container">
       <Header />
+      {/* add props in a component tag */}
       
     </div>
     // jsx expression must have one element
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
 // all component files start with a capitalized letter
 
 // import React from 'react' // used to be required
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
-            <header>
-                <h1>Task Tracker</h1>
+            <header className="container">
+                <h1>{title}</h1>
+                <button className="btn">Add</button>
             </header>
         )
+}
+
+Header.defaultProps = {
+    title: 'Tier List Maker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired, 
 }
 
 export default Header
